a20de4bd11f0e83238b45de05dc1eb6eac288b04
Alex Brykalov <alex.brykalov@gmail.com>
2016-06-03 13:42:40 +0300

    add dependency servlet to pom.xml and create UserServlet.java

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.web;
+
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+/**
+ * Created by Alex on 03.06.2016.
+ */
+public class UserServlet extends HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
