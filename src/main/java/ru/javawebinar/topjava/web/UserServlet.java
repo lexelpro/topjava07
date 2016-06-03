55f259051d6d4c40b355ce8502601f97540addef
Alex Brykalov <alex.brykalov@gmail.com>
2016-06-03 13:45:54 +0300

    Apply 1_4_add_servlet_api

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,17 +1,18 @@
 package ru.javawebinar.topjava.web;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Created by Alex on 03.06.2016.
+ * User: gkislin
+ * Date: 19.08.2014
  */
 public class UserServlet extends HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
-    }
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
     }
 }
