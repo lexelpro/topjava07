97dec55636c4b8eeabe976d61b5566deb87b0cee
Alex Brykalov <alex.brykalov@gmail.com>
2016-06-05 17:31:16 +0300

    add test data to doGet method

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,12 +2,17 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.UserMeal;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Alex on 05.06.2016.
@@ -20,9 +25,23 @@ public class MealServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("redirect to Meal List");
+        LOG.debug("open Meal List");
 
-        //request.getRequestDispatcher("/mealList.jsp").forward(request, response);
-        response.sendRedirect("mealList.jsp");
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+        );
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        request.setAttribute("mealList", mealList);
+
+
+        request.getRequestDispatcher("/mealList.jsp").forward(request, response);
+        //response.sendRedirect("mealList.jsp");
     }
 }
