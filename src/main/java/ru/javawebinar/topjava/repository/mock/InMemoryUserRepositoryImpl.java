ebc0fb67275812881a660e47a3c2da6f59cfb038
Alex Brykalov Mac <alex.brykalov@gmail.com>
2016-06-11 16:51:28 +0300

    rename MockUserRepositoryImpl in I nMemoryUserRepositoryImpl and fix create and delete method in InMemoryUserRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -8,31 +8,46 @@ import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * GKislin
  * 15.06.2015.
  */
 @Repository
-public class MockUserRepositoryImpl implements UserRepository {
-    private static final Logger LOG = LoggerFactory.getLogger(MockUserRepositoryImpl.class);
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+
+    private Map<Integer,User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
 
     @Override
     public boolean delete(int id) {
         LOG.info("delete " + id);
-        return true;
+        if (repository.containsKey(id)) {
+            repository.remove(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public User save(User user) {
         LOG.info("save " + user);
+        if (user.isNew())
+            user.setId(counter.incrementAndGet());
+        repository.put(user.getId(), user);
         return user;
     }
 
     @Override
     public User get(int id) {
         LOG.info("get " + id);
-        return null;
+
+        return repository.get(id);
     }
 
     @Override
