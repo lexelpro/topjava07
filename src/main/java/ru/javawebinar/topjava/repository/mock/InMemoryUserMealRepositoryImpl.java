93dd2c6e0b8c16360aa3c928bdfbbbcfab98c465
Alex Brykalov <alex.brykalov@gmail.com>
2016-06-11 19:34:08 +0300

    small fix

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserMealRepositoryImpl.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.repository.mock;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
 import ru.javawebinar.topjava.util.UserMealsUtil;
@@ -8,12 +11,16 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * GKislin
  * 15.09.2015.
  */
 public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserMealRepositoryImpl.class);
+
+
     private Map<Integer, UserMeal> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
@@ -23,6 +30,8 @@ public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
 
     @Override
     public UserMeal save(UserMeal userMeal) {
+        LOG.info("Save " + userMeal );
+
         if (userMeal.isNew()) {
             userMeal.setId(counter.incrementAndGet());
         }
@@ -32,17 +41,27 @@ public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
 
     @Override
     public void delete(int id) {
+        LOG.info("delete " + id);
         repository.remove(id);
     }
 
     @Override
     public UserMeal get(int id) {
+        LOG.info("get " + id);
+        if (repository.size() == 0)
+            return null;
         return repository.get(id);
     }
 
     @Override
     public Collection<UserMeal> getAll() {
-        return repository.values();
+        LOG.info("getAll");
+        if (repository.size() == 0)
+            return null;
+        return repository.entrySet().stream().
+                sorted(Map.Entry.comparingByValue((um2, um1) -> um1.getDateTime().compareTo(um2.getDateTime()))).
+                map( Map.Entry :: getValue).
+                collect(Collectors.toList());
     }
 }
 
