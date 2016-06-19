cbf2693506ac0dce54a8227360ac40e7876ded5d
Alex Brykalov <alex.brykalov@gmail.com>
2016-06-19 19:07:51 +0300

    add spring-db.xml to context

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -19,14 +19,14 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/spring-db.xml")) {
             System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            System.out.println(adminUserController.create(UserTestData.USER));
+          //  System.out.println(adminUserController.create(UserTestData.USER));
             System.out.println();
 
             UserMealRestController mealController = appCtx.getBean(UserMealRestController.class);
-            List<UserMealWithExceed> filteredMealsWithExceeded =
+           List<UserMealWithExceed> filteredMealsWithExceeded =
                     mealController.getBetween(
                             LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
                             LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
