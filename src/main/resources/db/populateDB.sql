5914ce1f7301b096ad14b519a003d9c354dc350e
Alex Brykalov <alex.brykalov@gmail.com>
2016-06-20 18:36:15 +0300

    create and add demo data to TABLE meal

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -11,3 +12,11 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, datetime, user_id) VALUES
+  ('Салат', 299, CURRENT_TIMESTAMP, 100000),
+  ('Каша', 400, CURRENT_TIMESTAMP, 100000),
+  ('Шоколад', 300, CURRENT_TIMESTAMP, 100000),
+  ('Салат админа', 299, CURRENT_TIMESTAMP, 100001),
+  ('Каша админа', 400, CURRENT_TIMESTAMP, 100001),
+  ('Шоколад админа', 300, CURRENT_TIMESTAMP, 100001);
